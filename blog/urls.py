c9f408172b977a0b533d6520ed56f012a036f343
ALee008 <nomansland008@googlemail.com>
2021-05-22 15:58:41 +0200

    Added view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    # specify an url pattern post/<int:pk>
+    # post/ means that the URL should begin with the word post followed by a /. So far so good.
+    # <int:pk> – this part is trickier. It means that Django expects an integer value and will transfer it
+    #            to a view as a variable called pk.
+    # / – then we need a / again before finishing the URL.
+    path('post/<int:pk>', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -6,9 +6,18 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     """
-
     :param request: Everything we receive from the user via the internet.
     :return:
     """
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    """
+    :param request: Everything we receive from the user via the internet.
+    :param pk: number of post
+    :return:
+    """
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
